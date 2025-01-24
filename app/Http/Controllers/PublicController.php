a086b4a1c64afb37eb725892144cb5ef932e16ab
opilane <opilane@tpt.lan>
2025-01-24 14:51:06 +0200

    add comments crud functional

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Like;
 use App\Models\Post;
 use App\Models\Tag;
+use App\Models\Comment;
 use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
@@ -21,6 +22,9 @@ class PublicController extends Controller
         return view('post', compact('post'));
     }
 
+    public function comment(Comment $comment){
+        return view('comment', compact('comment'));
+    }
     public function like(Post $post){
         $like = $post->likes()->where('user_id', auth()->user()->id)->first();
         if($like){
